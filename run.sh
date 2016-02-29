248aa323cd25c9cddecedd7f539cefc1d0040d7c
Zach Ramsay <zach@erisindustries.com>
2016-02-28 21:22:56 -0500

    cleanup run.sh

--- run.sh
+++ run.sh
@@ -6,17 +6,12 @@ sleep 2
 
 
 echo "Setting chain name:"
-CHAIN_NAME=toadserver_chn
-echo "$CHAIN_NAME"
-echo ""
-
-echo "Setting service name:"
-SERVICE_NAME=toadserver_srv
-echo "$SERVICE_NAME"
+chain_name=toadserver_chn
+echo "$chain_name"
 echo ""
 
 echo "Making key and genesis file"
-eris chains make --chain-type=simplechain $CHAIN_NAME
+eris chains make --chain-type=simplechain $chain_name
 
 echo "Getting address"
 echo ""
@@ -32,17 +27,17 @@ echo "$PUB"
 echo ""
 
 echo "Setting and chain directory:"
-CHAIN_DIR=$HOME/.eris/chains/$CHAIN_NAME
-echo "$CHAIN_DIR"
+chain_dir=$HOME/.eris/chains/$chain_name 
+echo "$chain_dir"
 echo ""
 
-echo "Copying default config to "$CHAIN_DIR"/default.toml"
+echo "Copying default config to "$chain_dir"/default.toml"
 echo ""
-cp ~/.eris/chains/default/config.toml $CHAIN_DIR/
+cp ~/.eris/chains/default/config.toml $chain_dir/
 
 echo "Starting chain"
 echo ""
-eris chains new $CHAIN_NAME --dir $CHAIN_DIR -p
+eris chains new $chain_name --dir $chain_dir -p
 sleep 2
 
 echo "Setting service definition file in:"
@@ -56,47 +51,9 @@ echo "Setting TOADSERVER_IPFS_NODES"
 NODES="" #give IPs where toadserver is running
 echo "$NODES"
 
-# TODO use existing servDef;
-# and pass in env vars via --env on start
-read -r -d '' SERV_DEF << EOM
-name = "$SERVICE_NAME"
-chain = "\$chain:toad:l"
-
-[service]
-name = "$SERVICE_NAME"
-image = "quay.io/eris/toadserver:latest"
-ports = [ "11113:11113" ]
-volumes = [  ]
-environment = [  
-"MINTX_NODE_ADDR=http://toad:46657/",
-"MINTX_CHAINID=$CHAIN_NAME", 
-"MINTX_SIGN_ADDR=http://keys:4767",
-"MINTX_PUBKEY=$PK",
-"ERIS_IPFS_HOST=http://ipfs",
-"TOADSERVER_IPFS_NODES=$NODES"
-]
-
-[dependencies]
-services = [ "ipfs", "keys" ]
-
-[maintainer]
-name = "Eris Industries"
-email = "support@erisindustries.com"
-
-[location]
-repository = "github.com/eris-ltd/toadserver"
-
-[machine]
-include = [ "docker" ]
-requires = [ "" ]
-EOM
-
-echo "$SERV_DEF" > "$HOME/.eris/services/${SERVICE_NAME}.toml"
-
 echo "Starting toadserver"
 echo ""
-eris services start $SERVICE_NAME --chain=$CHAIN_NAME
-sleep 5
+eris services start toadserver --chain=$chain_name --env "MINTX_CHAINID=$chain_name" --env "MINTX_PUBKEY=$PK"
 
 echo "Toadserver started"
 
