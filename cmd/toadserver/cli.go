9d387ece0517ad8daec0415a877404e753f8d25c
zramsay <zach@erisindustries.com>
2016-05-09 20:39:11 -0400

    some re-org to support a cli

---
+++ cmd/toadserver/cli.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/spf13/cobra"
+
+func startServer(cmd *cobra.Command, args []string) {
+}
+
+func putFiles(cmd *cobra.Command, args []string) {
+}
+
+func getFiles(cmd *cobra.Command, args []string) {
+}
---
+++ cmd/toadserver/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	
+	log "github.com/Sirupsen/logrus"
+	logger "github.com/eris-ltd/common/go/log"
+	
+	"github.com/spf13/cobra"
+)
+
+func main()
+	BuildToadserverCommand()
+	//Toadserver.PersistenPreRun = before
+	//Toadserver.PersistenPostRun = after
+	Toadserver.Execute()
+}
+
+var Toadserver = &cobra.Command{
+	Use: "toadserver",
+	Short: "",
+	Long: "",
+	Run: func(cmd *cobra.Command, args []string) { cmd.Help() },
+}
+
+func BuildToadserverCommand() {
+	Toadserver.AddCommand(startCmd)
+	Toadserver.AddCommand(putCmd)
+	Toadserver.AddCommand(getCmd)
+
+	addToadserverFlags()
+}
+
+func addToadserverFlags() {
+}
+
+var startCmd = &cobra.Command{
+	Use: "start",
+	Short: "",
+	Long: "",
+	Run: startServer,
+}
+
+var putCmd = &cobra.Command{
+	Use: "put",
+	Short: "",
+	Long: "",
+	Run: putFiles,
+}
+
+var getCmd = &cobra.Command{
+	Use: "get",
+	Short: "",
+	Long: "",
+	Run: getFiles,
+}
+
+func before(cmd *cobra.Command, args []string) {
+	log.SetFormatter(logger.ErisFormatter{})
+}
